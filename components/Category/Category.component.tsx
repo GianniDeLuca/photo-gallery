bd21751109ce0ddd4ab07c3f9f0ceb686753559a
GianniDeLuca <giannideluca93@gmail.com>
2022-07-30 20:33:05 -0300

    Add category route

--- components/Category/Category.component.tsx
+++ components/Category/Category.component.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import Image from "next/image";
 
+import { useRouter } from "next/router";
+
 import { Grid, Typography } from "@mui/material";
 
 interface ICategory {
@@ -10,8 +12,17 @@ interface ICategory {
 }
 
 export const Category: FC<ICategory> = ({ categoryTitle, categoryImage }) => {
+  const router = useRouter();
+
   return (
-    <Grid item xs={12} mb={2}>
+    <Grid
+      onClick={() => {
+        router.push(`/categories/${categoryTitle}`);
+      }}
+      item
+      xs={12}
+      mb={2}
+    >
       <Image
         src={categoryImage}
         height={300}
---
+++ pages/categories/[category].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { BaseLayout } from "layouts/BaseLayout";
+
+const CategoryPage: NextPage = () => {
+  return <BaseLayout>Category</BaseLayout>;
+};
+
+export default CategoryPage;
