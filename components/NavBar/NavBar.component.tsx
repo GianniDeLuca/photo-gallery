272a8550eaf319415f936af16968dfd73be49463
GianniDeLuca <giannideluca93@gmail.com>
2022-08-01 17:52:18 -0300

    Use box default props

--- components/NavBar/NavBar.component.tsx
+++ components/NavBar/NavBar.component.tsx
@@ -6,13 +6,7 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 export const NavBar = () => {
   const router = useRouter();
   return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "space-between",
-      }}
-      mb={2}
-    >
+    <Box display="flex" justifyContent="space-between" mb={2}>
       <Typography onClick={() => router.push("/")}>Full Name</Typography>
       <MenuOutlinedIcon />
     </Box>
