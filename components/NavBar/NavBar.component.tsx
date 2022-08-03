effd17630ecf25bc7e5d76ca12209468ba05e483
GianniDeLuca <giannideluca93@gmail.com>
2022-08-03 15:49:22 -0300

    vertical align navBar

--- components/NavBar/NavBar.component.tsx
+++ components/NavBar/NavBar.component.tsx
@@ -6,7 +6,12 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 export const NavBar = () => {
   const router = useRouter();
   return (
-    <Box display="flex" justifyContent="space-between" mb={2}>
+    <Box
+      display="flex"
+      justifyContent="space-between"
+      alignItems="center"
+      mb={2}
+    >
       <Typography onClick={() => router.push("/")}>Full Name</Typography>
       <MenuOutlinedIcon />
     </Box>
